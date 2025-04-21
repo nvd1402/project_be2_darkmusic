04f507f51b0d80e7251d1408e4079bc71362f915
LeHongSon-2005 <23211tt4339@mail.tdc.edu.vn>
2025-04-21 16:15:09 +0700

    Delete DoAnBE2 directory

--- DoAnBE2/resources/views/admin/partials/head.blade.php
+++
@@ -1,8 +0,0 @@
-
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-
-    <title>Dark Music</title>
