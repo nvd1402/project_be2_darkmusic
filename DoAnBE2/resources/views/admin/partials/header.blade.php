e589add37e9f557f6cfb76d693b6c286a97bec16
nvd1402 <nguyenvandai89@gmail.com>
2025-04-24 12:09:21 +0700

    header_new

--- DoAnBE2/resources/views/admin/partials/header.blade.php
+++ DoAnBE2/resources/views/admin/partials/header.blade.php
@@ -1,30 +1,3 @@
 <header>
-<<<<<<< Updated upstream
-    <div class="nav-links">
-        <button class="menu-btn" id="menu-open">
-            <i class='bx bx-menu'></i>
-        </button>
-    </div>
-
-    <!-- Form tìm kiếm -->
-    <div class="search">
-        <i class='bx bx-search'></i>
-
-        <!-- Tìm kiếm người dùng -->
-        <form action="{{ route('admin.users.search') }}" method="GET">
-            <input type="text" name="query" placeholder="" value="{{ request()->query('query') }}">
-        </form>
-
-{{--        <!-- Tìm kiếm bài hát -->--}}
-{{--        <form action="{{ route('admin.song.search') }}" method="GET">--}}
-{{--            <input type="text" name="query" placeholder="Tìm kiếm bài hát" value="{{ request()->query('query') }}">--}}
-{{--        </form>--}}
-
-
-
-    </div>
-
-=======
->>>>>>> Stashed changes
     <div class="userimg"></div>
 </header>
