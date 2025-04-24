feedd98bf3fdd8ab2ca529814abc5f9dd944c34d
vukha12 <kha15964@gmail.com>
2025-04-24 12:34:59 +0700

    done

--- DoAnBE2/resources/views/admin/artist/index.blade.php
+++ DoAnBE2/resources/views/admin/artist/index.blade.php
@@ -37,8 +37,8 @@
                         <td>{{ $artist->id }}</td>
                         <td>{{ $artist->name_artist }}</td>
                         <td>{{ $artist->genre }}</td>
-                        <td><a href={{ route('admin.artist.update',['id'=> $artist->id]) }} class="btn btn-primary">Sửa</a>|
-                            <a href={{ route('admin.artist.delete',['id' => $artist->id]) }} class="btn">Xoá</a> </td>
+                        <td><a href={{ route('admin.artist.update',['id'=> $artist->id]) }} class="link--artist">Sửa</a> |
+                            <a href={{ route('admin.artist.delete',['id' => $artist->id]) }} class="link--artist">Xoá</a> </td>
                     </tr>
                 @endforeach 
                 </tbody>
