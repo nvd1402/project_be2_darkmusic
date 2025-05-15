367d3007118d5c1509c12a2f8911642d2b6812c3
Chack05 <aduc4453@gmail.com>
2025-05-15 17:54:31 +0700

    update phase 2

--- DoAnBE2/resources/views/admin/partials/head.blade.php
+++ DoAnBE2/resources/views/admin/partials/head.blade.php
@@ -4,5 +4,6 @@
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/frontend/css/adminSong.css') }}">
 
     <title>Dark Music</title>
--- DoAnBE2/resources/views/frontend/song.blade.php
+++ DoAnBE2/resources/views/frontend/song.blade.php
@@ -1,18 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>@include('frontend.partials.head') </head>
-<body class="text-light">>
+<head>
+    @include('frontend.partials.head')
+</head>
+<body class="text-light">
 <div class="container">
-    <!-- Sidebar -->
     @include('frontend.partials.sidebar')
     <main>
-        <!-- navLink -->
         <header>
             <div class="nav-links">
                 <button class="menu-btn" id="menu-open">
                     <i class='bx bx-menu'></i>
                 </button>
-
             </div>
 
             <div class="search">
@@ -21,71 +20,74 @@
             </div>
         </header>
 
-            <div class="songs-recommendations">
-                <h2>Tất Cả Bài Hát</h2>
+        <div class="songs-recommendations">
+            <h2>Tất Cả Bài Hát</h2>
+            <div id="songs-list-container">
                 @foreach($songs as $song)
-                <ul class="songs-list" style="margin-bottom: 10px;">
-                    <li class="songs-item">
-                        @if($song->anh_daidien)
-                            <img src="{{ asset('storage/' . $song->anh_daidien) }}" width="50" alt="images">
-                        @else
-                            Không có ảnh
-                        @endif
-                        <div style="margin-right: 100px">
-                            <span>{{ $song->tenbaihat }}</span>
-                            <br>
-                            <small><span>{{ $song->artist->name_artist }}</span></small>
-                        </div>
-                        <span style="text-align: center">{{ $song->theloai }}</span>
-                            <td>
-                                @if($song->file_amthanh)
-                                    <audio controls>
-                                        <source src="{{ asset('storage/'.$song->file_amthanh) }}" type="audio/mpeg">
-                                        Your browser does not support the audio element.
-                                    </audio>
+                    <div class="songs-list songs-item-js">
+                        <li class="songs-item">
+                            <div class="song-thumbnail">
+                                @if($song->anh_daidien)
+                                    <img src="{{ asset('storage/' . $song->anh_daidien) }}" width="50" alt="images">
                                 @else
-                                    <p>Không có âm thanh</p>
-                                @endif
-                            </td>
-                    </li>
-                </ul>
+                                    <img src="https://via.placeholder.com/50" width="50" alt="no image"> @endif
+                            </div>
+
+                            <div class="song-details">
+                                <span>{{ $song->tenbaihat }}</span>
+                                <br>
+                                <small><span>{{ $song->artist->name_artist }}</span></small>
+                            </div>
+
+                            <div class="song-genre">
+                                <span style="text-align: center">{{ $song->category ? $song->category->tentheloai : 'Không có thể loại' }}</span>
+                            </div>
+
+                            <div class="song-audio">
+                                <audio id="audio-{{ $song->id }}" src="{{ asset('storage/'. $song->file_amthanh) }}"></audio>
+                                <div class="audio-controls">
+                                    <button class="play-pause-button" data-audio-id="audio-{{ $song->id }}">
+                                        <i class="fas fa-play"></i> </button>
+                                    <span class="audio-duration">0:00 / 0:00</span>
+                                </div>
+                            </div>
+                        </li>
+                    </div>
                 @endforeach
             </div>
-
-
-            <div class="artist-trending">
-                <h2>Các bản nhạc thịnh hành của 92914</h2>
-                <ul class="songs-list">
-                    <li class="songs-item">
-                        <span class="rank">1</span>
-                        <img src="./assets/trend.png" alt="">
-                        <span>Okinawa</span>
-
-                        <span>5:48</span>
-                    </li>
-                    <li class="songs-item">
-                        <span class="rank">2</span>
-                        <img src="./assets/trend.png" alt="">
-                        <span>Sunset</span>
-
-                        <span>3:12</span>
-                    </li>
-                    <li class="songs-item">
-                        <span class="rank">3</span>
-                        <img src="./assets/trend.png" alt="">
-                        <span>Koh</span>
-
-                        <span>5:21</span>
-                    </li>
-                </ul>
+            <div class="pagination-controls" id="pagination-controls">
             </div>
+        </div>
+
+        <div class="artist-trending">
+            <h2>Các bản nhạc thịnh hành của 92914</h2>
+            <ul class="songs-list">
+                <li class="songs-item">
+                    <span class="rank">1</span>
+                    <img src="./assets/trend.png" alt="">
+                    <span>Okinawa</span>
+                    <span>5:48</span>
+                </li>
+                <li class="songs-item">
+                    <span class="rank">2</span>
+                    <img src="./assets/trend.png" alt="">
+                    <span>Sunset</span>
+                    <span>3:12</span>
+                </li>
+                <li class="songs-item">
+                    <span class="rank">3</span>
+                    <img src="./assets/trend.png" alt="">
+                    <span>Koh</span>
+                    <span>5:21</span>
+                </li>
+            </ul>
+        </div>
         </section>
     </main>
     @include('frontend.partials.right_content')
-
-</div>
 </div>
 
 <script type='text/javascript' src="script.js"></script>
+<script type='text/javascript' src="{{ asset('assets/frontend/js/songs.js') }}"></script>
 </body>
 </html>
